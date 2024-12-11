92bd09b2fcee78af0bdb362ac943add0a54cd6de
Connor <mussonconnor@gmail.com>
2024-12-10 20:06:00 -0500

    fixed error

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -12,7 +12,7 @@ const sideNavOptions = [
 
 const SideNav = () => {
   return (
-    <aside className="text-white h-screen w-16 hidden small:flex flex-col items-center justify-between p-4 py-7">
+    <aside className="text-white h-screen w-16 hidden sm:flex flex-col items-center justify-between p-4 py-7">
       <div className="flex flex-col items-center gap-8">
         {sideNavOptions.map((option, idx) => (
           <IconButton
